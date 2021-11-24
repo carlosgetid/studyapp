dbfedb20ccd6f466445ac475be7c3cf7920ff47e
paulable <carlospgp13@gmail.com>
2021-11-24 09:28:18 -0500

    Project configuration

---
+++ src/main/java/com/cpgp/studyapp/StudyappApplication.java
@@ -0,0 +1,13 @@
+package com.cpgp.studyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyappApplication.class, args);
+	}
+
+}
